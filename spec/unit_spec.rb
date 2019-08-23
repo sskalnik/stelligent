72e42f2ef9eb5c42391040fe848307c9dcc47e85
Shelby Skalnik <sskalnik@sskalnik.com>
2019-08-23 18:27:46 +0000

    Add tests for invalid path (404) and server port per environment/stage

--- spec/unit_spec.rb
+++ spec/unit_spec.rb
@@ -3,6 +3,7 @@ require 'json'
 require 'rspec'
 require 'rack/test'
 require_relative '../stelligent'
+require 'thin' if ENV['RACK_ENV'] != 'development'
 
 RSpec.configure do |config|
   config.include Rack::Test::Methods
@@ -16,18 +17,36 @@ describe StelligentMiniProject do
   let(:response) { get '/' }
 
   # Intentionally use two different approaches to formatting `it...expect` statements just to show what's possible
-  context 'get "/"' do
+  context 'when getting "/"' do
     it { expect(response.status).to eq 200 }
     it { expect(response.body).to include "Automation for the People" }
   end
 
-  context 'returns the required JSON message' do
-    it 'has a content type of JSON' do
-      expect(response.headers["Content-Type"]).to include 'json'
+  context 'when getting "/some_invalid_path/"' do
+    let(:response) { get '/some_invalid_path/' }
+    it { expect(response.status).not_to eq  200 }
+    it { expect(response.status.to_s).to match /4\d\d/ }
+    it { expect(response.status).to eq 404 }
+  end
+
+  context 'when in development or test' do
+    it 'runs on port 8888' do
+      server = Thin::Server.new('0.0.0.0', '8888', app)
+      expect(server.port.to_i).to eq 8888
     end
   end
 
-  context 'timestamp' do
+  context 'when in deploy or production' do
+    it 'runs on port 8080' do
+      server = Thin::Server.new('0.0.0.0', '8080', app)
+      expect(server.port.to_i).to eq 8080
+    end
+  end
+
+  context 'when returning the required JSON message' do
+    it 'has a content type of JSON' do
+      expect(response.headers["Content-Type"]).to include 'json'
+    end
     it 'uses an integer timestamp format' do
       expect(JSON.parse(response.body)['timestamp']).to be_a(Integer)
     end
